cca191f321235e2f7415a93a09958cc8b6a08a5a
Codemaico <devwithmiki@gmail.com>
2023-12-13 16:53:46 +0200

    js not loading quick!!

---
+++ site/index.js
@@ -0,0 +1,19 @@
+function getBooks() {
+    document.getElementById('output').innerHTML="";
+
+    // fetching the data from the api according to our input
+
+    fetch("http://openlibrary.org/search.json?q="+
+    document.getElementById('input').value)
+
+    .then(a=>a.json())
+    .then(response => {
+    for(var i = 0; i < 20; i ++){  // shows 20 top results
+        document.getElementById('output').innerHTML+='<h2>'
+        + response.docs[i].title+'</h2>'+response.docs[i].author_name
+        [0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i]
+        .isbn[0]+"-M.jpg'><br>";
+    }
+
+    });
+}
