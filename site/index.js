349d02d62f306223ba02da0dd05d14f8c386d7c6
Codemaico <152258441+Codemaico@users.noreply.github.com>
2023-12-13 23:21:57 +0200

    Update index.js

--- site/index.js
+++ site/index.js
@@ -3,15 +3,12 @@ function getBooks() {
 
     // fetching the data from the api according to our input
 
-    fetch("https://openlibrary.org/swagger/docs#/search/read_search_json_search_json_get"+
-    document.getElementById('input').value)
+    fetch("https://openlibrary.org/swagger/docs#/search/read_search_json_search_json_get"+document.getElementById('input').value)
 
     .then(a=>a.json())
     .then(response => {
     for(var i = 0; i < 20; i ++){  // shows 20 top results
-        document.getElementById('output').innerHTML+='<h2>'
-        + response.docs[i].title+'</h2>'+response.docs[i].author_name
-        [0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i]
+        document.getElementById('output').innerHTML+='<h2>'+response.docs[i].title+'<h2>'+response.docs[i].author_name[0]+"<br><imgsrc='http://covers.openlibrary.org/b/isbn/"+response.docs[i]
         .isbn[0]+"-M.jpg'><br>";
     }
 
