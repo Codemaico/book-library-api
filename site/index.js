f50a9d2e6af74ccdfebe1524c622c41cb79fdb3b
Codemaico <devwithmiki@gmail.com>
2023-12-14 08:58:01 +0200

    API works now.

--- site/index.js
+++ site/index.js
@@ -3,16 +3,12 @@ function getBooks() {
 
     // fetching the data from the api according to our input
 
-    fetch("http://openlibrary.org/search.json?q="+
-    document.getElementById('input').value)
+    fetch("http://openlibrary.org/search.json?q="+document.getElementById('input').value)
 
     .then(a=>a.json())
     .then(response => {
     for(var i = 0; i < 20; i ++){  // shows 20 top results
-        document.getElementById('output').innerHTML+='<h2>'
-        + response.docs[i].title+'</h2>'+response.docs[i].author_name
-        [0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i]
-        .isbn[0]+"-M.jpg'><br>";
+        document.getElementById('output').innerHTML+='<h2>'+response.docs[i].title+'</h2>'+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
     }
 
     });
