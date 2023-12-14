8a38cd7b80214b9e3057d620ac1622ae0bfad2d5
Codemaico <devwithmiki@gmail.com>
2023-12-14 10:42:04 +0200

    JS issue

--- site/index.js
+++ site/index.js
@@ -3,7 +3,7 @@ function getBooks() {
 
     // fetching the data from the api according to our input
 
-    fetch("http://openlibrary.org/search.json?q="+document.getElementById('input').value)
+    fetch("https://openlibrary.org/search.json?q="+document.getElementById('input').value)
 
     .then(a=>a.json())
     .then(response => {
