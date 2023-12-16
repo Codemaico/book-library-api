995d5255fa669f125d947826b02d170f740e8f5f
Codemaico <devwithmiki@gmail.com>
2023-12-16 20:47:26 +0200

    shelf

--- site/index.js
+++ site/index.js
@@ -7,7 +7,7 @@ function getBooks() {
 
     .then(a=>a.json())
     .then(response => {
-    for(var i = 0; i < 20; i ++){  // shows 20 top results
+    for(var i = 0; i < 10; i ++){  // shows 20 top results
         document.getElementById('output').innerHTML+="<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>"+'<h2>'+response.docs[i].title+'</h2>';
     }
 
