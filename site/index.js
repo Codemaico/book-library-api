ba67fe58078b62e2619e5d261afc475399b326f4
Codemaico <devwithmiki@gmail.com>
2023-12-23 11:35:29 +0200

    pagination

--- site/index.js
+++ site/index.js
@@ -1,67 +1,123 @@
 // Keep track of displayed ISBNs to avoid duplicates
 const displayedISBNs = new Set();
 
-function getBooks() {
-    document.getElementById('output').innerHTML = "";
+// Keep track of the current page
+let currentPage = 1;
 
+// Setting the number of results per page
+const resultsPerPage = 10;
+
+let apiUrl;
+
+let results;
+function getBooks() {
     // Get the user input and author name
-    var userInput = document.getElementById('input').value;
-    var authorName = document.getElementById('authorInput').value;
+    const userInput = document.getElementById('input').value;
+    const authorName = document.getElementById('authorInput').value;
 
     // Construct the API query URL with both the user input and author name
-    var apiUrl = "https://openlibrary.org/search.json?q=" + userInput;
+    apiUrl = "https://openlibrary.org/search.json?q=" + userInput;
     if (authorName) {
-        apiUrl += "+author:" + authorName;
+        apiUrl +=  "+author:" + authorName;
     }
+
+    // Calculate the start index for pagination
+    const startIndex = (currentPage - 1) * resultsPerPage;
 
     // Fetch data from the Open Library API based on the modified query
     fetch(apiUrl)
-        .then(a => a.json())
-        .then(response => {
-            for (var i = 0; i < 50; i++) {
-                var currentISBN = response.docs[i].isbn[0];
+        .then(response => response.json())
+        .then(data => {
+            // Get a subset of results based on pagination
+            results = data.docs.slice(startIndex, startIndex + resultsPerPage);
 
-                // Check if ISBN is already displayed, skip if true
-                if (displayedISBNs.has(currentISBN)) {
-                    continue;
-                }
-
-                // Create a container for each item
-                var itemContainer = document.createElement('div');
-                itemContainer.className = 'item';
-
-                // Display book cover
-                var img = document.createElement('img');
-                img.src = 'http://covers.openlibrary.org/b/isbn/' + currentISBN + '-M.jpg';
-                img.alt = response.docs[i].title;
-
-                // Display book title
-                var h2 = document.createElement('h2');
-                h2.textContent = response.docs[i].title;
-
-                // Append the image and title to the container
-                itemContainer.appendChild(img);
-                itemContainer.appendChild(h2);
-
-                // Append the container to the output div
-                document.getElementById('output').appendChild(itemContainer);
-
-                // Add the ISBN to the set of displayed ISBNs
-                displayedISBNs.add(currentISBN);
+            // Display the results
+            displayResults(data, results);
+           
+            // Add pagination if there are more results
+            if (results.length < data.numFound) {
+                addPaginationButton();
             }
-
-            // Adjust the form container height after getting results
-            adjustFormHeight();
         });
 }
 
+function displayResults(data, results) {
+
+    console.log("Results:", data.numFound);
+    // Get the output element
+    const outputElement = document.getElementById('output');
+
+    // Clear the output
+    outputElement.innerHTML = "";
+
+    // Display each result
+    results.forEach(result => {
+        const currentISBN = result.isbn[0];
+
+        // Check if ISBN is already displayed, skip if true
+        if (displayedISBNs.has(currentISBN)) {
+            return;
+        }
+
+        // Create a container for each item
+        const itemContainer = document.createElement('div');
+        itemContainer.className = 'item';
+
+        // Display book cover
+        const img = document.createElement('img');
+        img.src = 'http://covers.openlibrary.org/b/isbn/' + currentISBN + '-M.jpg';
+        img.alt = result.title;
+
+        // Display book title
+        const h2 = document.createElement('h2');
+        h2.textContent = result.title;
+
+        // Append the image and title to the container
+        itemContainer.appendChild(img);
+        itemContainer.appendChild(h2);
+
+        // Append the container to the output div
+        outputElement.appendChild(itemContainer);
+
+        // Add the ISBN to the set of displayed ISBNs
+        displayedISBNs.add(currentISBN);
+    });
+
+    
+   
+}
+
+function addPaginationButton() {
+
+    
+    // Get the output element
+    const outputContainer = document.getElementById('output-container');
+
+    // Create a pagination button
+    const paginationButton = document.createElement('button');
+    paginationButton.textContent = "Load More";
+    paginationButton.classList.add('pagination-button');
+    
+    // Attach a click event to load more results
+    paginationButton.addEventListener('click', () => {
+        currentPage++;
+        getBooks();
+       
+    });
+
+    // Append the button to the output div
+    outputContainer.appendChild(paginationButton);
+}
+
+
+
 // Set the scroll threshold when to switch from fixed to sticky
-const scrollThreshold = 50;
+const scrollThreshold = 10;
 
 // Add a scroll event listener to handle form scrolling
-window.addEventListener('scroll', function() {
-    var formContainer = document.querySelector('.form-container');
-    var scrolled = window.scrollY;
+window.addEventListener('scroll', function () {
+    const formContainer = document.querySelector('.form-container');
+    const scrolled = window.scrollY;
 
     // Check if the scroll position has reached the threshold
     if (scrolled >= scrollThreshold) {
@@ -74,4 +130,3 @@ window.addEventListener('scroll', function() {
         formContainer.style.top = '0';
     }
 });
-
